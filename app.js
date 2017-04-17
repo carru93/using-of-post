e1424357d207e4c7c8deaecf1d80081b2aad408c
Emanuele <emanuele@localhost.localdomain>
2017-04-17 09:15:47 +0000

    quasi fatto, #login non funziona

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname+'/spublic')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(1234, function(req, res){
+  console.log('listening on 1234');
+});
+
+app.post('/login', function(req, res){
+
+});
